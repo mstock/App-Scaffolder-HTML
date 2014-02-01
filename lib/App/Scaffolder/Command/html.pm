bb4f4b54eecadde48ee7ed1d1417b7c1d1fb56fe
Manfred Stock <mstock@cpan.org>
2014-02-01 19:04:44 +0100

    Add initial version of App-Scaffolder-HTML

---
+++ lib/App/Scaffolder/Command/html.pm
@@ -0,0 +1,94 @@
+package App::Scaffolder::Command::html;
+use parent qw(App::Scaffolder::Command);
+
+# ABSTRACT: Scaffold a basic HTML file
+
+use strict;
+use warnings;
+
+use MRO::Compat;
+use Path::Class::Dir;
+
+=head1 SYNOPSIS
+
+	# Create a simple HTML5 file:
+	$ scaffolder html --template html5 --name index --title "My Document"
+
+	# Create a simple XHTML file:
+	$ scaffolder html --template xhtml --name index --title "My Document"
+
+=head1 DESCRIPTION
+
+App::Scaffolder::Command::html scaffolds HTML files. By default, it provides two
+simple templates:
+
+=over
+
+=item *
+
+C<html5>: Create a HTML5 document.
+
+=item *
+
+C<xhtml>: Create a XHTML document.
+
+=back
+
+=head1 METHODS
+
+=cut
+
+=head2 get_variables
+
+Specialized C<get_variables> version which returns the name of the output file
+and the title for the document.
+
+=cut
+
+sub get_variables {
+	my ($self, $opt) = @_;
+	return {
+		name  => scalar $opt->name(),
+		title => scalar $opt->title() || scalar $opt->name(),
+	};
+}
+
+
+=head2 get_dist_name
+
+Return the name of the dist this command is in.
+
+=cut
+
+sub get_dist_name {
+	return 'App-Scaffolder-HTML';
+}
+
+
+=head2 get_options
+
+Return additional options for this command.
+
+=cut
+
+sub get_options {
+	my ($class) = @_;
+	return (
+		[ 'name|n=s' => 'Name of the new HTML file that should be created, without suffix' ],
+		[ 'title=s'  => 'Title of the new HTML document that should be created' ],
+	);
+}
+
+
+sub validate_args {
+	my ($self, $opt, $args) = @_;
+
+	$self->next::method($opt, $args);
+	unless ($self->contains_base_args($opt) || $opt->name()) {
+		$self->usage_error("Parameter 'name' required");
+	}
+	return;
+}
+
+
+1;
---
+++ t/lib/App/Scaffolder/CommandTest/htmlTest.pm
@@ -0,0 +1,41 @@
+package App::Scaffolder::CommandTest::htmlTest;
+use parent qw(Test::Class);
+
+use strict;
+use warnings;
+
+use Carp;
+use Test::More;
+use Test::Exception;
+use App::Cmd::Tester;
+use Path::Class::Dir;
+use Test::File;
+use Test::File::ShareDir '-share' => {
+	'-dist' => { 'App-Scaffolder-HTML' => Path::Class::Dir->new(qw(share)) }
+};
+use Directory::Scratch;
+use App::Scaffolder;
+use App::Scaffolder::Command::html;
+
+sub app_test : Test(4) {
+	my ($self) = @_;
+
+	my $scratch = Directory::Scratch->new();
+	my $result = test_app('App::Scaffolder' => [
+		qw(html --name index --template html5 --target), $scratch->base()
+	]);
+	is($result->error, undef, 'threw no exceptions');
+	my $file = $scratch->base()->file('index.html');
+	file_exists_ok($file);
+
+	$scratch = Directory::Scratch->new();
+	$result = test_app('App::Scaffolder' => [
+		qw(html --name index --template xhtml10strict --target), $scratch->base()
+	]);
+	is($result->error, undef, 'threw no exceptions');
+	$file = $scratch->base()->file('index.xhtml');
+	file_exists_ok($file);
+}
+
+
+1;
